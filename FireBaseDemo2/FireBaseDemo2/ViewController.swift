6138e41d98f41b4a8ae49e272806d2f7272233d4
HoangNV <hoangnv.paraline@gmail.com>
2016-12-17 07:37:24 +0700

    firebasedemo2

---
+++ FireBaseDemo2/FireBaseDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseDemo2
+//
+//  Created by Hoang Nguyen on 12/17/16.
+//  Copyright © 2016 Hoang Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
